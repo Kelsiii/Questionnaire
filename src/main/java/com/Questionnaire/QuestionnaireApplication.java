ba5182a5bfa148bb3649980512d7d269c4a28080
Kelsiii <wdjwjzjy@163.com>
2017-12-28 18:33:01 +0800

    for local development

--- src/main/java/com/Questionnaire/QuestionnaireApplication.java
+++ src/main/java/com/Questionnaire/QuestionnaireApplication.java
@@ -13,22 +13,6 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 
-@ServletComponentScan
-@SpringBootApplication
-public class QuestionnaireApplication extends SpringBootServletInitializer{  
-	  
-  
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(QuestionnaireApplication.class, args);
-    }
-
-    @Override  
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {  
-        return builder.sources(this.getClass());  
-    }  
-  
-} 
-/*
 @SpringBootApplication
 public class QuestionnaireApplication {
 	
@@ -37,5 +21,5 @@ public class QuestionnaireApplication {
 		SpringApplication.run(QuestionnaireApplication.class, args);
 	}
 }
-*/
+
 
